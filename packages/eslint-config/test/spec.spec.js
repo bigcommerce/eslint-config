75d8aa03da3be5a00238271195e6908a8e4b32da
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2024-05-09 13:28:18 +0200

    test: add check for deprecated rules

--- packages/eslint-config/test/spec.spec.js
+++ packages/eslint-config/test/spec.spec.js
@@ -15,3 +15,52 @@ it('checks that the rules are working', async () => {
 
   expect(result.messages).toEqual([]);
 });
+
+it('checks for rule deprecations', async () => {
+  const [result] = await getEslint().lintFiles([fileName]);
+
+  expect(result.usedDeprecatedRules).toMatchInlineSnapshot(`
+    [
+      {
+        "replacedBy": [],
+        "ruleId": "global-require",
+      },
+      {
+        "replacedBy": [
+          "padding-line-between-statements",
+        ],
+        "ruleId": "lines-around-directive",
+      },
+      {
+        "replacedBy": [],
+        "ruleId": "no-buffer-constructor",
+      },
+      {
+        "replacedBy": [
+          "no-object-constructor",
+        ],
+        "ruleId": "no-new-object",
+      },
+      {
+        "replacedBy": [],
+        "ruleId": "no-new-require",
+      },
+      {
+        "replacedBy": [],
+        "ruleId": "no-path-concat",
+      },
+      {
+        "replacedBy": [],
+        "ruleId": "no-return-await",
+      },
+      {
+        "replacedBy": [],
+        "ruleId": "padding-line-between-statements",
+      },
+      {
+        "replacedBy": [],
+        "ruleId": "spaced-comment",
+      },
+    ]
+  `);
+});
